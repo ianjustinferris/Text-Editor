2992cd47127bef33877308a0c9aabdb377abd63f
ianjustinferris <ianjustinferris@gmail.com>
2022-07-10 16:37:56 -0400

    Added GET/POST logic to get and add content to db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,28 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('PUT to db');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content })
+  const result = await request;
+  console.log('data saved to database');
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  result ? console.log('data retrieved from database', result.value)
+    : console.log('data not found in database', result.value)
+
+  return result?.value
+}
 
 initdb();
